76bfc6a857344f8f5107023da571be8acee4ba91
alis <ali_drn_333@hotmail.com>
2022-10-26 23:18:13 +0300

    .1,6
    :(

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,35 @@
 import { NavLink } from 'react-router-dom'
+import {Home} from 'views/Home.js'
 
-function Section({ title, more = false }) {
+function Section({ title, more = false, items }) {
   return (
     <section>
-      <header className="flex items-center justify-between">
+      <header className="mb-4 flex items-center justify-between">
         <h3 className="text-2xl text-white font-semibold tracking-tight">
           {title}
         </h3>
         {more && (
-          <NavLink className={'text-sm font-semibold uppercase'} to={more}>
+          <NavLink
+            className={
+              'text-xs hover:underline font-semibold uppercase text-link'
+            }
+            to={more}
+          >
             HEPSİNİ GÖR
           </NavLink>
         )}
       </header>
-    </section>
+
+
+      <div className="grid grid-cols-4">{items.map((item=>(
+
+        <NavLink key={item.id} to="/" >
+          {item.title}
+        </NavLink>
+
+      ))},</div>
+
+      </section>
   )
 }
 export default Section
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,38 @@
 import Section from '../../src/components/Section'
 
 function Home() {
+  const items = [
+    {
+      id: 1,
+      title: 'Yeni nesil insan hakları',
+      description: 'Ağu 2021',
+      image: 'https://i.scdn.co/image/81eb55c7633dcdc418268f3698eb3394b35e3bd4',
+    },
+    {
+      id: 2,
+      title: 'Yeni nesil insan hakları',
+      description: 'Ağu 2021',
+      image: 'https://i.scdn.co/image/81eb55c7633dcdc418268f3698eb3394b35e3bd4',
+    },
+    {
+      id: 3,
+      title: 'Yeni nesil insan hakları',
+      description: 'Ağu 2021',
+      image: 'https://i.scdn.co/image/81eb55c7633dcdc418268f3698eb3394b35e3bd4',
+    },
+    {
+      id: 4,
+      title: 'Yeni nesil insan hakları',
+      description: 'Ağu 2021',
+      image: 'https://i.scdn.co/image/81eb55c7633dcdc418268f3698eb3394b35e3bd4',
+    },
+  ]
   return (
     <div>
       <Section
         title="Dinleyicileri arasında popüler: Evrim Ağacı ile Bilime Dair Her Şey!"
         more="/blabla"
+        items={items}
       />
     </div>
   )
