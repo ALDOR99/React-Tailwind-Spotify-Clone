6e5c710a80421bcbbc88f6fad64f3642538dc0db
alis <ali_drn_333@hotmail.com>
2022-10-28 18:41:01 +0300

    .1,7
    :apple:

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,6 +2,17 @@ import { NavLink } from 'react-router-dom'
 import { Icon } from 'Icons'
 
 function Section({ title, more = false, items }) {
+  const imageStyle = (item) => {
+    switch (item.type) {
+      case 'artist':
+        return 'rounded-full'
+      case 'podcast':
+        return 'rounded-lg'
+      default:
+        return 'rounded'
+    }
+  }
+
   return (
     <section>
       <header className="mb-4 flex items-center justify-between">
@@ -23,16 +34,18 @@ function Section({ title, more = false, items }) {
       <div className="grid grid-cols-7 gap-x-6">
         {items.map((item) => (
           <NavLink
-            className={'bg-footer p-4 rounded hover:bg-active'}
+            className={'bg-footer p-4 rounded hover:bg-active group'}
             key={item.id}
             to="/"
           >
             <div className="pt-[100%] relative mb-4">
               <img
                 src={item.image}
-                className="absolute top-0 inset-0 object-cover w-full h-full"
+                className={`absolute top-0 inset-0 object-cover w-full h-full ${imageStyle(
+                  item,
+                )}`}
               />
-              <button className="w-10 h-10 rounded-full bg-primary absolute bottom-2 right-2 flex items-center justify-center">
+              <button className="w-10 h-10 rounded-full bg-primary absolute group-hover:flex group-focus:flex bottom-2 right-2 items-center justify-center hidden">
                 <Icon name="play" size={16} />
               </button>
             </div>
