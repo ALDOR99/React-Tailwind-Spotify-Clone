99497f31048276ffa81759ad33390e5c6ee0f22b
alis <ali_drn_333@hotmail.com>
2022-11-03 22:58:30 +0300

    .1,13
    :alien:

--- src/stores/player.js
+++ src/stores/player.js
@@ -9,29 +9,29 @@ const initialState: CounterState = {
   current: false,
   controls: false,
   playing: false,
-}
+}+
 
 export const playerSlice = createSlice({
   name: 'player',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
+    setCurrent:(state,action) => {
+      state.current = action.payload
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    },
+    setControls:(state,action) => {
+      state.controls = action.payload
+    },  
+    setPlaying:(state,action) => {
+      state.playing = action.payload
+    },       
+
   },
 })
 
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const {
+  setCurrent,
+  setControls,
+  setPlaying
+} = playerSlice.actions
 
-export default counterSlice.reducer
+export default playerSlice.reducer
