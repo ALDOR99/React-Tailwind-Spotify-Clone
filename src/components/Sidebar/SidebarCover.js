3d0779f6e16665086b3abf6712fb8acb0c77b1ce
alis <ali_drn_333@hotmail.com>
2022-11-11 12:53:24 +0300

    :1,16
    :alien:

--- src/components/Sidebar/SidebarCover.js
+++ src/components/Sidebar/SidebarCover.js
@@ -1,5 +1,15 @@
+import { useSelector } from 'react-redux'
+
 function SidebarCover() {
-  return <div className="pt-[100%] bg-white"></div>
+  const current = useSelector((state) => state.player.current)
+  return (
+    <div className="pt-[100%] relative bg-black">
+      <img
+        src={current.image}
+        className="w-full h-full object-cover absolute top-0 left-0"
+      ></img>
+    </div>
+  )
 }
 
 export default SidebarCover
