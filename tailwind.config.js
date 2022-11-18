32d3087c69d15f431d7ad9c4ce02b86270b1e41e
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2022-11-18 12:19:20 +0300

    .1,18
    :alien:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        backdrop: '#121212',
-        primary: '#1db954',
-        active: '#282828',
-        link: '#b3b3b3',
-        footer: '#181818',
+        backdrop: "#121212",
+        primary: "#1db954",
+        active: "#282828",
+        link: "#b3b3b3",
+        footer: "#181818",
       },
       fontSize: {
-        s: '0.813rem',
+        s: "0.813rem",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
   ],
-}
+};
