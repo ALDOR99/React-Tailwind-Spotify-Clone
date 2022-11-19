ab811a16270fe3e224db0e2e1ad3c90863b92d65
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2022-11-19 22:12:17 +0300

    .1,19
    :alien:

--- src/components/Navbar/Search.js
+++ src/components/Navbar/Search.js
@@ -1,9 +1,20 @@
+import { Icon } from "Icons";
+
+//------------------------------------------------------------------------------------
+
 function Search() {
   return (
-    <div className="mr-auto ml-4 h-10">
+    <div className="mr-auto ml-4 relative">
+      <label
+        htmlFor="search-input"
+        className="text-black/70 w-11 h-10 flex items-center justify-center absolute top-0 left-0"
+      >
+        <Icon size={24} name="search" />
+      </label>
       <input
         type="text"
-        className="h-10 bg-white rounded-3xl"
+        id="search-input"
+        className="h-10 bg-white pl-12 outline-none text-black text-sm max-w-full w-[22.75rem] rounded-3xl placeholder-black/50"
         placeholder="Sanatçilar,şarkilar veya podcast'ler"
       />
     </div>
--- src/views/Search.js
+++ src/views/Search.js
@@ -1,4 +1,33 @@
+import categories from "data/categories";
+
+//----------------------------------------------------------------
+
+function Category({ category }) {
+  return (
+    <div
+      style={{ background: category.color }}
+      className="rounded-md before:pt-[100%] before:block relative"
+    >
+      <div className="absolute inset-0">
+        <h3 className="p-4 text-2xl tracking-tighter font-semibold">
+          {category.title}
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+//----------------------------------------------------------------
+
 function Search() {
-  return <div>Burasi arama sayfasi</div>;
+  return (
+    <div className="grid grid-cols-5 gap-6">
+      {categories.map((category) => (
+        <Category category={category} />
+      ))}
+    </div>
+  );
 }
 export default Search;
+
+//----------------------------------------------------------------
